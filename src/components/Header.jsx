3065df8169c508663b63dd885ce6d294a0921e91
greencode-dev <antonioverde.dev@gmail.com>
2025-12-15 20:34:40 +0100

    added header.jsx + header css

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+function Header() {
+  return (
+    <header>
+      <img id="logo" src="/img/dc-logo.png" alt="DC Logo" />
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Characters</a>
+          </li>
+          <li>
+            <a href="#">Comics</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">Games</a>
+          </li>
+          <li>
+            <a href="#">Collectibles</a>
+          </li>
+          <li>
+            <a href="#">Videos</a>
+          </li>
+          <li>
+            <a href="#">Fans</a>
+          </li>
+          <li>
+            <a href="#">News</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
